fc166992e91c6f5b44a5eb0506367833760fb530
Publikwerker <hello@publikwerks.com>
2019-09-16 14:00:35 -0700

    looking for bug

--- src/utils/stoTwi.js
+++ src/utils/stoTwi.js
@@ -5,6 +5,7 @@ export default function StoTwi(symbol, callback){
   let url = `${STOCKTWITS_API}/${symbol}.json`;
   request({url, json:true}, (err, response, body) => {
     if (err){
+      console.log(err);
       callback(`Unable to access network connection`);
     } else if (response.body.errors) {
       callback(response.body.errors[0].message);
