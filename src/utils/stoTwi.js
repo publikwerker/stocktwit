b1645dd2dd2187067bd7196b01fc178333131906
Publikwerker <hello@publikwerks.com>
2019-09-16 15:02:02 -0700

    set mode manually

--- src/utils/stoTwi.js
+++ src/utils/stoTwi.js
@@ -1,10 +1,9 @@
 const request = require('request');
-const cors = require('cors');
 const {STOCKTWITS_API} = require('../config');
 
 export default function StoTwi(symbol, callback){
   let url = `${STOCKTWITS_API}/${symbol}.json`;
-  request({url, json:true}, (err, response, body) => {
+  request({url, mode:'no-cors', json:true}, (err, response, body) => {
     if (err){
       console.log(err);
       callback(`Unable to access network connection`);
