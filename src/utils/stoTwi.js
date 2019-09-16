3fc4b52f55660e6a6264657cedc7d1ebb6dce204
Publikwerker <hello@publikwerks.com>
2019-09-16 14:24:45 -0700

    installed npm cors

--- src/utils/stoTwi.js
+++ src/utils/stoTwi.js
@@ -1,9 +1,10 @@
 const request = require('request');
+const cors = require('cors');
 const {STOCKTWITS_API} = require('../config');
 
 export default function StoTwi(symbol, callback){
   let url = `${STOCKTWITS_API}/${symbol}.json`;
-  request({url, json:true}, (err, response, body) => {
+  request({url, json:true}, cors(), (err, response, body) => {
     if (err){
       console.log(err);
       callback(`Unable to access network connection`);
